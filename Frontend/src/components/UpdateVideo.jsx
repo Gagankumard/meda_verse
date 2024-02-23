1ba26ac9116f589fdaa966b0638000b71dbfa417
beenow011 <abhinavnb11@gmail.com>
2024-02-23 17:45:10 +0530

    update video 2

--- Frontend/src/components/UpdateVideo.jsx
+++ Frontend/src/components/UpdateVideo.jsx
@@ -15,6 +15,33 @@ function UpdateVideo() {
   const thumbnailFileTypes = ["JPG", "PNG", "JPEG"];
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoading2, setIsLoading2] = useState(false);
+  const [title, setTitle] = useState(vid?.title);
+  const [desc, setDesc] = useState(vid?.description);
+  const [thumbnail, setThumbnail] = useState();
+  const [file, setFile] = useState(null);
+
+  const urlToFile = async (url) => {
+    try {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      return new File([blob], "thumbnail.jpg", { type: "image/jpeg" });
+    } catch (error) {
+      console.error("Error converting URL to file:", error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (vid?.thumbnail) {
+        const thumbnailFile = await urlToFile(vid.thumbnail);
+        setFile(thumbnailFile);
+      }
+    };
+    fetchData();
+  }, [vid?.thumbnail]);
+  // console.log("file", file);
   useEffect(() => {
     const getVideo = async () => {
       try {
@@ -29,7 +56,7 @@ function UpdateVideo() {
     getVideo();
   }, []);
   const handleChangeThumbnail = (file) => {
-    // setThumbnail(file);
+    setThumbnail(file);
   };
   const handleBack = () => {
     if (page === 1) {
@@ -38,13 +65,38 @@ function UpdateVideo() {
       setPage(1);
     }
   };
-  const handleSave = () => {
+  const handleSave = async () => {
     if (page === 1) {
       setPage(2);
     } else {
       console.log("saved");
+      try {
+        setIsLoading2(true);
+        const res = await axios.patch(
+          `/api/v1/videos/updateVideo/${vid?._id}`,
+          {
+            title: title ? title : vid?.title,
+            description: desc,
+            thumbnail: thumbnail,
+          },
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+        if (res?.data?.success) {
+          console.log("res", res?.data);
+          setIsLoading2(false);
+          navigate("/");
+        }
+      } catch (err) {
+        setIsLoading2(false);
+        console.log(err.response);
+      }
     }
   };
+  console.log(title?.trim() !== "" ? title : vid?.title);
   const handleDelete = async () => {
     try {
       setIsLoading(true);
@@ -91,7 +143,9 @@ function UpdateVideo() {
               className="bg-violet-400/60 mb-3 text-white"
               label="Title"
               variant="outlined"
+              value={title}
               placeholder={vid?.title}
+              onChange={(e) => setTitle(e.target.value)}
               size="small"
               fullWidth
             />
@@ -112,11 +166,12 @@ function UpdateVideo() {
             size="small"
             multiline
             fullWidth
+            rows={4}
+            onChange={(e) => setDesc(e.target.value)}
             placeholder={vid?.description.slice(0, 20)}
             inputProps={{ maxLength: 100 }}
             InputProps={{
               style: {
-                overflowWrap: "break-word", // Wrap long words onto the next line
                 fontSize: "small",
               },
             }}
@@ -136,7 +191,15 @@ function UpdateVideo() {
           className="p-2 flex-1 flex justify-center items-center hover:bg-violet-700/70 transition cursor-pointer"
           onClick={handleSave}
         >
-          {page === 2 ? "Save" : "Next"}
+          {page === 2 ? (
+            isLoading2 ? (
+              <BeatLoader color="rgba(54, 99, 214, 1)" />
+            ) : (
+              "Save"
+            )
+          ) : (
+            "Next"
+          )}
         </div>
       </div>
       {page === 2 && (

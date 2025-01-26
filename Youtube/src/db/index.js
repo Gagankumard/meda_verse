edd3da67a571e2e863ea457f877fcf76dda3e84e
Gagankumard <gagank37851@gmail.com>
2025-01-26 16:47:52 +0530

    new page added

--- Youtube/src/db/index.js
+++ Youtube/src/db/index.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const uri = `mongodb+srv://abhinavnb11:benaka234@cluster0.vbw5nr9.mongodb.net`
-const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(uri)
-        console.log(`\n MongoDB connected!! DB  host: ${connectionInstance.connection.host}`)
-    } catch (err) {
-        console.log("MONGODB connection error", err);
-        process.exit(1)
-    }
-}
+const uri = `mongodb+srv://gagankumardd39:7892407180@cluster0.z9n1e.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
-export default connectDB
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(uri);
+    console.log(
+      `\nMongoDB connected!! DB host: ${connectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.log("MONGODB connection error", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
